6879b74e6becf3437bc5c0f248f6d77ef2e89394
Noor Chasib <noorchasib@gmail.com>
2023-03-10 14:58:43 -0800

    update sidebar with profile picture

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -93,6 +93,7 @@ const navItems = [
 ];
 
 const Sidebar = ({
+	user,
 	drawerWidth,
 	isSidebarOpen,
 	setIsSidebarOpen,
@@ -145,12 +146,13 @@ const Sidebar = ({
 							{navItems.map(({ text, icon }) => {
 								if (!icon) {
 									return (
-										<Typography key={text} sx={{ m: "2rem 0 1rem 3rem" }}>
+										<Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
 											{text}
 										</Typography>
 									);
 								}
 								const lcText = text.toLowerCase();
+
 								return (
 									<ListItem key={text} disablePadding>
 										<ListItemButton
@@ -189,6 +191,42 @@ const Sidebar = ({
 								);
 							})}
 						</List>
+					</Box>
+
+					<Box position="absolute" bottom="2rem">
+						<Divider />
+						<FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+							<Box
+								component="img"
+								alt="profile"
+								src={profileImage}
+								height="40px"
+								width="40px"
+								borderRadius="50%"
+								sx={{ objectFit: "cover" }}
+							/>
+							<Box textAlign="left">
+								<Typography
+									fontWeight="bold"
+									fontSize="0.9rem"
+									sx={{ color: theme.palette.secondary[100] }}
+								>
+									{user.name}
+								</Typography>
+								<Typography
+									fontSize="0.8rem"
+									sx={{ color: theme.palette.secondary[200] }}
+								>
+									{user.occupation}
+								</Typography>
+							</Box>
+							<SettingsOutlined
+								sx={{
+									color: theme.palette.secondary[300],
+									fontSize: "25px ",
+								}}
+							/>
+						</FlexBetween>
 					</Box>
 				</Drawer>
 			)}
