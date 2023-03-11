2d7e9e04f5de7050637f22ebecd4342b72d7a408
Noor Chasib <noorchasib@gmail.com>
2023-03-10 18:14:36 -0800

    create products page component

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ import salesRoutes from "./routes/sales.js";
 
 // data imports
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js"
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
 
 // Configuration
 dotenv.config();
@@ -34,20 +36,20 @@ app.use("/sales", salesRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    /* ONLY ADD DATA ONE TIME */
-    // AffiliateStat.insertMany(dataAffiliateStat);
-    // OverallStat.insertMany(dataOverallStat);
-    // Product.insertMany(dataProduct);
-    // ProductStat.insertMany(dataProductStat);
-    // Transaction.insertMany(dataTransaction);
-    
-    // User.insertMany(dataUser);
-  })
-  .catch((error) => console.log(`${error} did not connect`));
+		/* ONLY ADD DATA ONE TIME */
+		// AffiliateStat.insertMany(dataAffiliateStat);
+		// OverallStat.insertMany(dataOverallStat);
+		// Transaction.insertMany(dataTransaction);
+
+		// Product.insertMany(dataProduct);
+		// ProductStat.insertMany(dataProductStat);
+		// User.insertMany(dataUser);
+	})
+	.catch((error) => console.log(`${error} did not connect`));
