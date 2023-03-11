bd6113da7a25765a17bfa932c52d1d4993b6d872
Noor Chasib <noorchasib@gmail.com>
2023-03-10 21:39:46 -0800

    Re-linting

--- server/models/AffiliateStat.js
+++ server/models/AffiliateStat.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const AffiliateStatSchema = new mongoose.Schema(
-  {
-    userId: { type: mongoose.Types.ObjectId, ref: "User" },
-    affiliateSales: {
-      type: [mongoose.Types.ObjectId],
-      ref: "Transaction",
-    },
-  },
-  { timestamps: true }
+	{
+		userId: { type: mongoose.Types.ObjectId, ref: "User" },
+		affiliateSales: {
+			type: [mongoose.Types.ObjectId],
+			ref: "Transaction",
+		},
+	},
+	{ timestamps: true }
 );
 
 const AffiliateStat = mongoose.model("AffiliateStat", AffiliateStatSchema);
